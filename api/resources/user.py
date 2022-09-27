c0bfb1b586a6820589f41b64ef38078e99b5d926
adamhsu2501 <adamhsu2501@gmail.com>
2022-09-27 10:39:02 +0800

    Fix: Set-cookie with secure=True and samesite=None

--- api/resources/user.py
+++ api/resources/user.py
@@ -50,7 +50,7 @@ class LoginUser(UserBaseResource):
             # create response
             response = self.handle_success_response(data=data)
             response.set_cookie(SESSION_ID_NAME, session_cookie,
-                                expires=expires, httponly=True, secure=False, samesite=None)
+                                expires=expires, httponly=True, secure=True, samesite=None)
              
             return response
         except Exception as e:
